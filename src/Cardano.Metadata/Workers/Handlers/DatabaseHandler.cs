3d67bbe2d18690899b63db789c63bb652f7c4e8f
Herminigildo L. Timtim <hermitimtim23@gmail.com>
2025-03-21 03:50:28 +0800

    feat: added github worker

---
+++ src/Cardano.Metadata/Workers/Handlers/DatabaseHandler.cs
@@ -0,0 +1,103 @@
+using System.Text.Json;
+using Cardano.Metadata.Data;
+using Cardano.Metadata.Models.Entity;
+using Cardano.Metadata.Models.Response;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cardano.Metadata.Workers.Handlers;
+
+public class DatabaseHandler(ILogger<Github> logger, IDbContextFactory<MetadataDbContext> dbContextFactory)
+{
+    private readonly ILogger<Github> _logger = logger;
+    private readonly IDbContextFactory<MetadataDbContext> _dbContextFactory = dbContextFactory;
+
+    public async Task UpdateSyncStateAsync(string sha, DateTime? commitDate, CancellationToken cancellationToken)
+    {
+        using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
+        await dbContext.SyncState.AddAsync(new SyncState(
+            sha ?? string.Empty,
+            commitDate ?? DateTimeOffset.UtcNow
+        ), cancellationToken);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+    }
+
+    public async Task<MetaData> GetOrCreateTokenAsync(JsonElement mappingJson, string subject, CancellationToken cancellationToken)
+    {
+        using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
+
+        var existingToken = await dbContext.MetaData
+            .FirstOrDefaultAsync(tm => tm.Subject != null && tm.Subject.ToLower() == subject.ToLower(), cancellationToken);
+
+        var registryItem = CreateTokenMetadata(mappingJson, subject);
+
+        if (string.IsNullOrEmpty(registryItem.Subject) || registryItem.Decimals == null || string.IsNullOrEmpty(registryItem.Name?.Value) || string.IsNullOrEmpty(registryItem.Ticker?.Value))
+        {
+            _logger.LogWarning("Invalid data for token. Name, Ticker, or Subject cannot be null or empty.");
+            return null;
+        }
+
+        var token = new MetaData(
+            registryItem.Subject,
+            registryItem.Name.Value,
+            registryItem.Ticker.Value,
+            registryItem.Subject.Substring(0, 56),
+            registryItem.Decimals.Value,
+            registryItem.Policy ?? null,
+            registryItem.Url?.Value ?? null,
+            registryItem.Logo?.Value ?? null,
+            registryItem.Description?.Value ?? null
+        );
+
+        if (existingToken != null)
+        {
+            _logger.LogDebug("Updated token: {subject}", subject);
+        }
+
+        return token;
+    }
+
+    public async Task<SyncState?> GetLatestSyncStateAsync(CancellationToken cancellationToken)
+    {
+        using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
+        return await dbContext.SyncState
+            .OrderByDescending(ss => ss.Date)
+            .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+    }
+
+    private static RegistryItem CreateTokenMetadata(JsonElement mappingJson, string subject)
+    {
+        var registryItem = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(mappingJson.GetRawText());
+
+        if (registryItem == null)
+        {
+            throw new InvalidOperationException("Failed to deserialize mappingJson into a Dictionary.");
+        }
+
+        var name = registryItem.ContainsKey("name") ? registryItem["name"].GetProperty("value").GetString() : null;
+        var ticker = registryItem.ContainsKey("ticker") ? registryItem["ticker"].GetProperty("value").GetString() : null;
+        var description = registryItem.ContainsKey("description") ? registryItem["description"].GetProperty("value").GetString() : null;
+        var policy = registryItem.ContainsKey("policy") ? registryItem["policy"].GetString() : null;
+        var url = registryItem.ContainsKey("url") ? registryItem["url"].GetProperty("value").GetString() : null;
+        var logo = registryItem.ContainsKey("logo") ? registryItem["logo"].GetProperty("value").GetString() : null;
+        var decimals = registryItem.ContainsKey("decimals") ? registryItem["decimals"].GetProperty("value").GetInt32() : 0;
+
+        var result = new RegistryItem
+        {
+            Subject = subject,
+            Policy = policy,
+            Name = new ValueResponse<string> { Value = name },
+            Ticker = new ValueResponse<string> { Value = ticker },
+            Description = new ValueResponse<string> { Value = description },
+            Url = new ValueResponse<string> { Value = url },
+            Logo = new ValueResponse<string> { Value = logo },
+            Decimals = new ValueResponse<int> { Value = decimals }
+        };
+
+        return result;
+    }
+
+
+
+}
