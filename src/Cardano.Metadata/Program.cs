f71db3c6ca31917b2991cb4480e69d5eb431af0c
Herminigildo L. Timtim <hermitimtim23@gmail.com>
2025-03-22 00:48:16 +0800

    refactor: modified program.cs for github worker registration

--- src/Cardano.Metadata/Program.cs
+++ src/Cardano.Metadata/Program.cs
@@ -3,6 +3,9 @@ using Cardano.Metadata.Data;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using Cardano.Metadata.Modules.Handlers;
+using System.Net.Http.Headers;
+using System.Reflection;
+using Cardano.Metadata.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,24 @@ builder.Services.AddDbContextFactory<MetadataDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddSingleton<MetadataHandler>();
+builder.Services.AddSingleton<MetadataDbService>();
+builder.Services.AddSingleton<GithubService>();
+builder.Services.AddHostedService<GithubWorker>();
+
+builder.Services.AddHttpClient("GithubApi", client =>
+{
+    client.BaseAddress = new Uri("https://api.github.com/");
+    ProductInfoHeaderValue productValue = new("CardanoTokenMetadataService", Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "Unknown Version");
+    ProductInfoHeaderValue commentValue = new("(+https://github.com/SAIB-Inc/Cardano.Metadata)");
+    client.DefaultRequestHeaders.UserAgent.Add(productValue);
+    client.DefaultRequestHeaders.UserAgent.Add(commentValue);
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", builder.Configuration["GithubPAT"]);
+});
+
+builder.Services.AddHttpClient("GithubRaw", client =>
+{
+    client.BaseAddress = new Uri("https://raw.githubusercontent.com/");
+});
 
 var app = builder.Build();
 
@@ -23,7 +44,10 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+
 app.UseHttpsRedirection();
+
 app.MapCarter();
+
 
 app.Run();
